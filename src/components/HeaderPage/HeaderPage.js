13da28643c44d0f5b9f213b5035a3083d138d222
Danis Begishev <begishevdanis@gmail.com>
2020-12-15 13:10:56 +0300

    RequestPage added, header modified

--- src/components/HeaderPage/HeaderPage.js
+++ src/components/HeaderPage/HeaderPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
-
-import { Layout, Button } from 'antd';
+import { Link } from 'react-router-dom';
+import { Layout, Button, Menu } from 'antd';
 
 import './HeaderPage.scss';
 import logo from '../../images/logo.svg'; 
@@ -10,11 +10,21 @@ const { Header } = Layout;
 export const HeaderPage = (props) => {
 
     const extraMenu = () => (
-        <>
+        <>  
+            <Link
+                className="page-header__item"
+                to="/request">
+                Создать заявку
+            </Link>
+            <Link 
+                className="page-header__item"
+                to="/course-requests">
+                Курсы
+            </Link>
             <Button
                 className="page-header__button"
                 type="primary"
-                danger>
+            >
                 <span className="page-header__button-text">
                     {props.loggedIn ? 'Войти' : 'Выйти'}
                 </span>
@@ -26,7 +36,9 @@ export const HeaderPage = (props) => {
         <>  
             <Header>
                 <div className="page-header">
-                    <img src={logo} alt="logo" />
+                    <a href="/">
+                        <img src={logo} alt="logo" />
+                    </a>
                     <div>{extraMenu()}</div>
                 </div>
             </Header>
--- src/components/Request/Request.js
+++ src/components/Request/Request.js
@@ -4,53 +4,106 @@ import { Form, Input, Button } from 'antd';
 import './Request.scss';
 
 export const Request = () => {
+
+    const [form] = Form.useForm();
+
+    const sendForm = async (data) => {
+        const response = await fetch(``, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: data
+        }).catch(function(error){
+            console.log(error);
+        })
+        if (response){
+            const resData = await response.json();
+
+            if (resData.status === 'success'){
+               alert('Data sent'); 
+            }
+            else if (resData.status === 'fail'){
+                alert('Something went wrong');
+            }
+        }
+    }
+
     const handleSubmit = (values) => {
+        
+        // sendForm(values);
         console.log(values);
-        // alert(values.json());
+        form.resetFields();
     }
     
     return (
         <>  
             <div className="request">
                 <Form
+                    form={form}
                     layout="vertical"
                     onFinish={handleSubmit}
-                >   
-                    <Form.Item
-                        name="username"
-                        label="ФИО">
-                        <Input />
-                    </Form.Item>
-                    <Form.Item
-                        name="coursename"
-                        label="Название курса">
-                        <Input />
-                    </Form.Item>
-                    <Form.Item
-                        name="link"
-                        label="Ссылка на курс">
-                        <Input />
-                    </Form.Item>
-                    <Form.Item
-                        name="price"
-                        label="Цена курса (в рублях)">
-                        <Input type="number" />
-                    </Form.Item>
-                    <Form.Item
-                        name="date"
-                        label="Дата начала курса">
-                        <Input type="date" />
-                    </Form.Item>
-                    <Form.Item
-                        name="quarter"
-                        label="Квартал обучения">
-                        <Input />
-                    </Form.Item>
-                    <Form.Item
-                        name="description"
-                        label="Обоснование">
-                        <Input.TextArea />
-                    </Form.Item>
+                >
+                    <div className="request__form-group">
+                        <Form.Item
+                            className="request__form-item"
+                            name="firstname"
+                            label={<label className="request__form-label">Фамилия</label>}
+                            required>
+                            <Input className="request__form-input"/>
+                        </Form.Item>
+                        <Form.Item
+                            className="request__form-item"
+                            name="lastname"
+                            label={<label className="request__form-label">Имя</label>}
+                            required>
+                            <Input className="request__form-input"/>
+                        </Form.Item>
+                    </div>
+                    <div className="request__form-group">
+                        <Form.Item
+                            className="request__form-item"
+                            name="coursename"
+                            label={<label className="request__form-label">Название курса</label>}
+                            required>
+                            <Input className="request__form-input" />
+                        </Form.Item>
+                        <Form.Item
+                            className="request__form-item"
+                            name="link"
+                            label={<label className="request__form-label">Ссылка на курс</label>}
+                            required>
+                            <Input className="request__form-input" />
+                        </Form.Item>
+                    </div>
+                    <div className="request__form-group">
+                        <Form.Item
+                            className="request__form-item"
+                            name="price"
+                            label={<label className="request__form-label">Цена курса (в рублях)</label>}>
+                            <Input className="request__form-input" type="number" />
+                        </Form.Item>
+                        <Form.Item
+                            className="request__form-item"
+                            name="date"
+                            label={<label className="request__form-label">Дата начала курса</label>}>
+                            <Input className="request__form-input" type="date" />
+                        </Form.Item>
+                    </div>  
+                    <div className="request__form-group">
+                        <Form.Item
+                            className="request__form-item"
+                            name="quarter"
+                            label={<label className="request__form-label">Квартал обучения</label>}>
+                            <Input className="request__form-input" />
+                        </Form.Item>
+                        <Form.Item
+                            className="request__form-item"
+                            name="description"
+                            label={<label className="request__form-label">Обоснование</label>}>
+                            <Input.TextArea rows={3} />
+                        </Form.Item>
+                    </div>
                     <Form.Item>
                         <Button
                             className="request__button"
